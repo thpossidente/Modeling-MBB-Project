50cbb5eed5c37a48e2538b57faf06cfdd1e9200c
thposs <tposside@gmail.com>
2016-11-08 13:57:47 -0500

    Save

--- codes.R
+++ codes.R
@@ -1,17 +1,40 @@
 #Create function with parameter p that creates matrix of weights. 
 n.input1 <- 10
 n.input2 <- 10
-n.hidden <- 50
+n.hidden1 <- 25
+n.hidden2 <- 25
 n.output <- 10
 
-s <- matrix(0, nrow = 5, ncol = 5)
+rows1 <- 5
+rows2 <-5
+columns1 <- 6
+columns2 <-6
+s <- matrix(0, nrow = rows1+rows2, ncol = columns1+columns2)
 
+for(i in (columns1+1):(columns1+columns2)){
+  s[,i] <- s[,i] + 1
+}
+
+input1.and.input2.to.hidden.weights <- matrix(0, nrow = (n.input1 + n.input2), ncol = (n.hidden1 + n.hidden2))
+hidden.to.output <- matrix(0, nrow = (n.hidden1 + n.hidden2), ncol = n.output)
 
 level.of.integration <- function(p){ #p must be between 0 and 1
-  input1.and.input2.to.hidden.weights <- matrix(0, nrow = (n.input1+n.input2), ncol = n.hidden)
-  hidden.to.output <- matrix(0, nrow = n.hidden, ncol = n.output)
-  for(i in 1:n.input1.and.input2.to.hidden.weights){
-    
+  
+  for(i in 1:n.hidden1){
+    input1.and.input2.to.hidden.weights[i,] <- 
+      
+  }
+  
+  for(i in (n.hidden1+1):(n.hidden1+n.hidden2)){
+    input1.and.input2.to.hidden.weights[i,] <-
+  }
+  
+  for(i in 1:n.input1){
+    input1.and.input2.to.hidden.weights[,i] <-
+  }
+  
+  for(i in (n.input1+1):(n.input1+n.input2)){
+    input1.and.input2.to.hidden.weights[,i] <-
   }
 }
 
