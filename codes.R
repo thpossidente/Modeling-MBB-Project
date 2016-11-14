a46af61f41e66a5930a2ab6b686643421c70922e
thposs <tposside@gmail.com>
2016-11-13 23:57:50 -0500

    integration parameter working!

--- codes.R
+++ codes.R
@@ -4,39 +4,39 @@ n.input2 <- 10
 n.hidden1 <- 25
 n.hidden2 <- 25
 n.output <- 10
+p <- 0.5
 
-rows1 <- 5
-rows2 <-5
-columns1 <- 6
-columns2 <-6
-s <- matrix(0, nrow = rows1+rows2, ncol = columns1+columns2)
+input1.to.hidden1.weights <- matrix(runif((n.input1*n.hidden1), 0, 1), nrow = n.input1, ncol = n.hidden1)
+input2.to.hidden2.weights <- matrix(runif((n.input2*n.hidden2), 0, 1), nrow = n.input2, ncol = n.hidden2)
 
-for(i in (columns1+1):(columns1+columns2)){
-  s[,i] <- s[,i] + 1
-}
+hidden1.to.output.weights <- matrix(runif((n.hidden1*n.output), 0, 1), nrow = n.hidden1, ncol = n.output) 
+hidden2.to.output.weights <- matrix(runif((n.hidden2*n.output), 0, 1), nrow = n.hidden2, ncol = n.output)
 
-input1.and.input2.to.hidden.weights <- matrix(0, nrow = (n.input1 + n.input2), ncol = (n.hidden1 + n.hidden2))
-hidden.to.output <- matrix(0, nrow = (n.hidden1 + n.hidden2), ncol = n.output)
+
+input1.to.hidden2.weights <- matrix(0, nrow = n.input1, ncol = n.hidden2)
+input2.to.hidden1.weights <- matrix(0, nrow = n.input2, ncol = n.hidden2)
 
 level.of.integration <- function(p){ #p must be between 0 and 1
+
   
-  for(i in 1:n.hidden1){
-    input1.and.input2.to.hidden.weights[i,] <- 
-      
-  }
+  for(i in 1:length(input1.to.hidden2.weights)){
+    if(runif(1) <= p){
+      input1.to.hidden2.weights[i] <<- runif(1)
+    } else{
+        input1.to.hidden2.weights[i] <<- 0
+  }}
   
-  for(i in (n.hidden1+1):(n.hidden1+n.hidden2)){
-    input1.and.input2.to.hidden.weights[i,] <-
-  }
+  for(o in 1:length(input2.to.hidden1.weights)){
+    if(runif(1) <= p){
+      input2.to.hidden1.weights[o] <<- runif(1)
+    } else{
+        input2.to.hidden1.weights[o] <<- 0
+  }}
   
-  for(i in 1:n.input1){
-    input1.and.input2.to.hidden.weights[,i] <-
-  }
-  
-  for(i in (n.input1+1):(n.input1+n.input2)){
-    input1.and.input2.to.hidden.weights[,i] <-
-  }
 }
+
+level.of.integration(p)
+
 
 
 forward.pass <- function(input1, input2){
